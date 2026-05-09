6f0f6282a6970aef7f79ceb2262886b8907203df
Thomas Bray <87775189+MTG-Thomas@users.noreply.github.com>
2026-05-09 10:02:47 -0400

    Serve WinGet source information statically

--- scripts/prepare_site.ps1
+++ scripts/prepare_site.ps1
@@ -9,6 +9,7 @@ New-Item -ItemType Directory -Force $site | Out-Null
 Copy-Item (Join-Path $PSScriptRoot "..\\index.json") $site
 Copy-Item (Join-Path $PSScriptRoot "..\\index.html") $site
 Copy-Item (Join-Path $PSScriptRoot "..\\source.json") $site
+Copy-Item (Join-Path $PSScriptRoot "..\\information.json") $site
 Copy-Item (Join-Path $PSScriptRoot "..\\staticwebapp.config.json") $site
 Copy-Item -Recurse (Join-Path $PSScriptRoot "..\\packageManifests") $site
 Copy-Item -Recurse (Join-Path $PSScriptRoot "..\\manifests") $site
